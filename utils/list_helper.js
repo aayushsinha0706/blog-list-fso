412e0e288c9b7a2ca469c6b16bf7a70a75a2ea32
Aayush SInha <84586770+aayushsinha0706@users.noreply.github.com>
2024-12-20 00:43:25 +0530

    4.5*: Helper Functions and Unit Tests, step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
     return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const result = blogs.reduce((favourite, blog) => (blog.likes > favourite.likes ? blog : favourite ),blogs[0])
+    return result ? {title: result.title, author: result.author, likes: result.likes} : null
+}
+
+
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }

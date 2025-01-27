bbb4951e35993995f7d3e538e937ae50c53bfc81
Aayush SInha <84586770+aayushsinha0706@users.noreply.github.com>
2025-01-27 18:11:34 +0530

    4.13 Blog List Expansions, step 1

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -81,6 +81,20 @@ test('test create blog missing title or url returns 400', async () => {
        .expect(400)
 })
 
+test('deletion of a note', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+    
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map(blog => blog.title)
+    assert(!titles.includes(blogToDelete.title))
+})
 after( async () => {
     await mongoose.connection.close()
 })
